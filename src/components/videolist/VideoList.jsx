08649bef2c38a3ff3b5ff5b482117e96e1ef9573
cjaytechguy <cjaytechguy@gmail.com>
2021-01-05 23:09:23 +0100

    added the video list component

--- src/components/videolist/VideoList.jsx
+++ src/components/videolist/VideoList.jsx
@@ -1,10 +1,15 @@
 import React, { useContext } from 'react';
 import VideoContext from '../../context/VideoContext';
+import VideoItem from '../videoitem/VideoItem';
 
 const VideoList = () => {
   const { videos } = useContext(VideoContext);
 
-  return <div>{videos.length}</div>;
+  const renderedList = videos?.map((video, idx) => (
+    <VideoItem video={video} key={idx} />
+  ));
+
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
